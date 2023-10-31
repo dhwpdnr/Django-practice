b4984fcdc244481fd4567fce17c11a041b6abdf7
dhwpdnr <ink1237@gmail.com>
2023-10-31 09:42:22 +0900

    [feat] mannager set

--- managertest/models.py
+++ managertest/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 
+class ActiveManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(active=True)
+
+
 class Article(models.Model):
     """Article model Definition"""
     name = models.CharField(max_length=100)
     description = models.TextField()
     active = models.BooleanField(default=True)
+    active_objects = ActiveManager()
 
     def __str__(self):
         return self.name
@@ -16,6 +22,7 @@ class Magazine(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     active = models.BooleanField(default=True)
+    active_objects = ActiveManager()
 
     def __str__(self):
         return self.name
