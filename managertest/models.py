b046e0da0ee556898b2ead7cb02112cadabe4069
dhwpdnr <ink1237@gmail.com>
2023-11-01 13:34:52 +0900

    [feat] default manager

--- managertest/models.py
+++ managertest/models.py
@@ -6,12 +6,21 @@ class ActiveManager(models.Manager):
         return super().get_queryset().filter(active=True)
 
 
+class AbstractBaseManager(models.Model):
+    active_objects = ActiveManager()
+    objects = models.Manager()
+
+    class Meta:
+        abstract = True
+
+
 class Article(models.Model):
     """Article model Definition"""
     name = models.CharField(max_length=100)
     description = models.TextField()
     active = models.BooleanField(default=True)
     active_objects = ActiveManager()
+    objects = models.Manager()
 
     def __str__(self):
         return self.name
@@ -23,6 +32,7 @@ class Magazine(models.Model):
     description = models.TextField()
     active = models.BooleanField(default=True)
     active_objects = ActiveManager()
+    objects = models.Manager()
 
     def __str__(self):
         return self.name
