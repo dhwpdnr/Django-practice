d092aef118131423ec05f328f4a917c36b1cc659
dhwpdnr <ink1237@gmail.com>
2023-11-05 20:45:15 +0900

    [feat] django manager custom

--- managertest/models.py
+++ managertest/models.py
@@ -32,3 +32,31 @@ class Magazine(AbstractBase):
 
     def __str__(self):
         return self.name
+
+
+class PersonManager(models.Manager):
+    def author(self):
+        return self.get_queryset().filter(role="A")
+
+    def editor(self):
+        return self.get_queryset().filter(role="E")
+
+
+class ActivePersonManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(active=True)
+
+    def author(self):
+        return self.get_queryset().filter(role="A")
+
+    def editor(self):
+        return self.get_queryset().filter(role="E")
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    role = models.CharField(max_length=1, choices=[("A", ("Author")), ("E", ("Editor"))])
+    active = models.BooleanField(default=True)
+    people = PersonManager()
+    active_people = ActivePersonManager()
