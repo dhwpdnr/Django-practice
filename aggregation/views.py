f517ea8a22deba2490f96a739ba3092fd61aa2bc
dhwpdnr <ink1237@gmail.com>
2023-11-26 20:29:28 +0900

    [feat] aggregate test

--- aggregation/views.py
+++ aggregation/views.py
@@ -1,11 +1,22 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Book
-from django.db.models import Avg
+from django.db.models import Avg, Sum, Max, F, FloatField
 
 
 class AggregateTestAPI(APIView):
     def get(self, request):
         price_avg = Book.objects.all().aggregate(Avg("price"))
-
-        return Response(data={})
+        total_price = Book.objects.all().aggregate(Sum("price"))
+        max_price = Book.objects.all().aggregate(Max("price"))
+        price_per_page = Book.objects.all().aggregate(
+            price_per_page=Sum(F("price") / F("pages"), output_field=FloatField())
+        )
+        return Response(
+            data={
+                "price_avg": price_avg,
+                "total_price": total_price,
+                "max_price": max_price,
+                "price_per_page": price_per_page,
+            }
+        )
