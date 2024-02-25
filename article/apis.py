a62d8ac67bc32826d5cc2474c17f1c0066d3fb12
dhwpdnr <ink1237@gmail.com>
2024-02-25 18:15:13 +0900

    [feat] swagger parameter

--- article/apis.py
+++ article/apis.py
@@ -1,7 +1,8 @@
 from rest_framework import generics, viewsets
 from .seriializers import ArticleSerializer, ArticleCreateSerializer
 from .models import Article
-from drf_spectacular.utils import extend_schema, OpenApiExample
+from drf_spectacular.utils import extend_schema, OpenApiExample, OpenApiParameter
+from drf_spectacular.types import OpenApiTypes
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
@@ -9,17 +10,26 @@ class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
 
     @extend_schema(
-        request=ArticleCreateSerializer,
-        tags=["Article", "Create"],
-        examples=[
-            OpenApiExample(
-                "Create a new Article",
-                summary="Create a new Article",
-                value={
-                    "title": "New Article",
-                    "content": "New Article Content",
-                },
-            )
+        tags=["테스트"],
+        summary="method레벨 데코레이터도 가능",
+        parameters=[
+            OpenApiParameter(
+                name="a_param", description="QueryParam1 입니다.", required=False, type=str
+            ),
+            OpenApiParameter(
+                name="date_param",
+                type=OpenApiTypes.DATE,
+                location=OpenApiParameter.QUERY,
+                description="Filter by release date",
+                examples=[
+                    OpenApiExample(
+                        "이것은 Query Parameter Example입니다.",
+                        summary="short optional summary",
+                        description="longer description",
+                        value="1993-08-23",
+                    ),
+                ],
+            ),
         ],
     )
     def create(self, request, *args, **kwargs):
